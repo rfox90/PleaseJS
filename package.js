d6d07306c225ad82c5cd1a21eadb7a4db3b4b544
Richard Fox <richard.fox@ahref.co.uk>
2015-07-12 20:06:18 +0100

    Create package under my namespace for now, I'll PR later

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
 	summary: 'The Polite Javascript Color Library - makes a random pleasing color',
 	version: "1.0.0",
-	git: "https://github.com/Fooidge/PleaseJS",
-	name: "fooidge:pleasejs"
+	git: "https://github.com/rfox90/PleaseJS",
+	name: "ahref:pleasejs"
 });
 
 Package.on_use(function (api) {
